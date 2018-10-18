23710b4b4e8db0057a6b91465a4cd98763110727
Vincent van Proosdij <mini32@gmail.com>
2018-10-18 23:09:39 +0200

    init

--- tests/Sample.App/Configuration/NaosAppconfiguration.cs
+++ tests/Sample.App/Configuration/NaosAppconfiguration.cs
@@ -5,5 +5,12 @@
     public class NaosAppconfiguration : NaosCoreConfiguration
     {
         public AppConfiguration App { get; set; }
+
+        public class AppConfiguration
+        {
+            public string Name { get; set; }
+
+            public CosmosDbConfiguration CosmosDb { get; set; }
+        }
     }
 }
