6ae291cc805855ab7ecec6bd65b41b0cf307e241
Vincent van Proosdij <mini32@gmail.com>
2018-12-04 00:27:04 +0100

    GlobalExceptionFilter

---
+++ src/App.Web/Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,67 @@
+﻿namespace Naos.Core.App.Web.Filters
+{
+    using System.Diagnostics;
+    using FluentValidation;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using Microsoft.Extensions.Logging;
+    using Naos.Core.App.Correlation;
+    using Naos.Core.Common;
+    using Naos.Core.Common.Web;
+
+    public class GlobalExceptionFilter : IExceptionFilter
+    {
+        private readonly IHostingEnvironment env;
+        private readonly ILogger<GlobalExceptionFilter> logger;
+        private readonly CorrelationContextAccessor correlationContext;
+
+        public GlobalExceptionFilter(
+            IHostingEnvironment environment,
+            ILogger<GlobalExceptionFilter> logger,
+            CorrelationContextAccessor correlationContext)
+        {
+            this.env = environment;
+            this.logger = logger;
+            this.correlationContext = correlationContext;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            this.logger.LogError(new EventId(context.Exception.HResult),
+                context.Exception,
+                context.Exception?.Message);
+
+            if (context.Exception is ValidationException exception)
+            {
+                var details = new ValidationProblemDetails
+                {
+                    //Title = $"the request for {{this.serviceDescriptor > ACCESSOR}} has validation errors, refer to the errors property for details",
+                    Title = "a validation error occurred",
+                    Instance = $"{this.correlationContext.Context.CorrelationId} ({this.correlationContext.Context.RequestId})",
+                    //Detail = context.HttpContext.Request.Path,
+                    Type = exception.GetType().FullPrettyName(),
+                    Status = 400
+                };
+
+                (context.Exception as ValidationException)?.Errors.NullToEmpty().ForEach(f => details.Errors.Add(f.PropertyName, new[] { f.ToString() }));
+                context.Result = new BadRequestObjectResult(details);
+            }
+            else
+            {
+                context.Result = new InternalServerErrorObjectResult(
+                    new ProblemDetails
+                    {
+                        Title = "an unexpected error occurred",
+                        Instance = $"{this.correlationContext.Context.CorrelationId} ({this.correlationContext.Context.RequestId})",
+                        Type = context.Exception.GetType().FullPrettyName(),
+                        Status = 500,
+                        Detail = context.Exception.Demystify().ToString()
+                    });
+                //context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            }
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
---
+++ src/Common.Web/InternalServerErrorObjectResult.cs
@@ -0,0 +1,18 @@
+﻿namespace Naos.Core.Common.Web
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// Custom action result for internal server errors (500)
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.ObjectResult" />
+    public class InternalServerErrorObjectResult : ObjectResult
+    {
+        public InternalServerErrorObjectResult(object error)
+            : base(error)
+        {
+            this.StatusCode = StatusCodes.Status500InternalServerError;
+        }
+    }
+}
