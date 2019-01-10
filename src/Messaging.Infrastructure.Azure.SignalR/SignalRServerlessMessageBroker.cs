0a491e73b5faac3c6dc50a2a6f0dd3c1c511ac1c
Vincent van Proosdij <mini32@gmail.com>
2019-01-10 22:20:24 +0100

    tmp

--- src/Messaging.Infrastructure.Azure.SignalR/SignalRServerlessMessageBroker.cs
+++ src/Messaging.Infrastructure.Azure.SignalR/SignalRServerlessMessageBroker.cs
@@ -3,6 +3,8 @@
     using System;
     using System.Collections.Generic;
     using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.SignalR.Client;
     using Naos.Core.Messaging.Domain.Model;
 
     public class SignalRServerlessMessageBroker : IMessageBroker
@@ -10,6 +12,8 @@
         public void Publish(Message message)
         {
             // TODO: publish by http posting to hub endpoint https://github.com/aspnet/AzureSignalR-samples/blob/master/samples/Serverless/ServerHandler.cs
+            //       hubname = 'naos_messaging' (TestMessage)
+            //       group = messageName
             throw new NotImplementedException();
         }
 
@@ -18,7 +22,26 @@
             where THandler : IMessageHandler<TMessage>
         {
             // TODO: subscribe by connecting to the hub and registering SendMessage handler (=ProcessMessage)
+            //       hubname = 'naos_messaging' (TestMessage)
+            //       group = messageName
             //       https://github.com/aspnet/AzureSignalR-samples/blob/master/samples/Serverless/ClientHandler.cs
+
+            //var _connection = new HubConnectionBuilder()
+            //    .WithUrl(url, option =>
+            //    {
+            //        option.AccessTokenProvider = () =>
+            //        {
+            //            return Task.FromResult(serviceUtils.GenerateAccessToken(url, userId));
+            //        };
+            //    }).Build();
+
+            //_connection.On<string, string>(
+            //    "SendMessage",
+            //    (string server, string message) =>
+            //    {
+            //        Console.WriteLine($"[{DateTime.Now.ToString()}] Received message from server {server}: {message}");
+            //    });
+
             throw new NotImplementedException();
         }
 
