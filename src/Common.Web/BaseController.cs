6a141a619f9fd2143c455436b1156e50a7b6d241
Vincent van Proosdij <mini32@gmail.com>
2019-02-05 00:04:54 +0100

    build fix

--- src/Common.Web/BaseController.cs
+++ src/Common.Web/BaseController.cs
@@ -1,14 +1,14 @@
-﻿namespace Naos.Core.Common.Web
-{
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.Extensions.DependencyInjection;
+﻿//namespace Naos.Core.Common.Web
+//{
+//    using Microsoft.AspNetCore.Mvc;
+//    using Microsoft.Extensions.DependencyInjection;
 
-    [ApiController]
-    [Route("api/[controller]/[action]")]
-    public abstract class BaseController : Controller
-    {
-        private IMediator _mediator;
+//    [ApiController]
+//    [Route("api/[controller]/[action]")]
+//    public abstract class BaseController : Controller
+//    {
+//        private IMediator _mediator;
 
-        protected IMediator Mediator => _mediator ?? (_mediator = HttpContext.RequestServices.GetService<IMediator>());
-    }
-}
+//        protected IMediator Mediator => _mediator ?? (_mediator = HttpContext.RequestServices.GetService<IMediator>());
+//    }
+//}
