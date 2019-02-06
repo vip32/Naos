17f7945e3b66c5c271b58b6c82c529be8b42bc4f
Vincent van Proosdij <mini32@gmail.com>
2019-02-07 00:47:01 +0100

    DataDictionary added (properties)

--- src/Common/Extensions/TypeExtensions.cs
+++ src/Common/Extensions/TypeExtensions.cs
@@ -52,5 +52,46 @@
 
             return source.FullName;
         }
+
+        public static bool IsNumeric(this Type type)
+        {
+            if (type.IsArray)
+            {
+                return false;
+            }
+
+            if (type == typeof(byte) ||
+                type == typeof(decimal) ||
+                type == typeof(double) ||
+                type == typeof(short) ||
+                type == typeof(int) ||
+                type == typeof(long) ||
+                type == typeof(sbyte) ||
+                type == typeof(float) ||
+                type == typeof(ushort) ||
+                type == typeof(uint) ||
+                type == typeof(ulong))
+            {
+                return true;
+            }
+
+            switch (Type.GetTypeCode(type))
+            {
+                case TypeCode.Byte:
+                case TypeCode.Decimal:
+                case TypeCode.Double:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                case TypeCode.SByte:
+                case TypeCode.Single:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                    return true;
+            }
+
+            return false;
+        }
     }
 }
---
+++ src/Domain/Model/DataDictionary.cs
@@ -0,0 +1,137 @@
+﻿namespace Naos.Core.Domain.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using Naos.Core.Common;
+
+    public class DataDictionary : Dictionary<string, object>
+    {
+        public static readonly DataDictionary Empty = new DataDictionary();
+
+        public DataDictionary()
+            : base(StringComparer.OrdinalIgnoreCase)
+        {
+        }
+
+        public DataDictionary(IEnumerable<KeyValuePair<string, object>> items)
+            : base(StringComparer.OrdinalIgnoreCase)
+        {
+            foreach (var item in items)
+            {
+                this.Add(item.Key, item.Value);
+            }
+        }
+
+        public DataDictionary AddOrUpdate<T>(string key, object value)
+        {
+            if (key.IsNullOrEmpty())
+            {
+                return this;
+            }
+
+            if (this.ContainsKey(key))
+            {
+                this.Remove(key);
+            }
+
+            this.Add(key, value);
+
+            return this;
+        }
+
+        public object GetValueOrDefault(string key)
+        {
+            if (key.IsNullOrEmpty())
+            {
+                return null;
+            }
+
+            return this.TryGetValue(key, out object value) ? value : null;
+        }
+
+        public object GetValueOrDefault(string key, object defaultValue)
+        {
+            if (key.IsNullOrEmpty())
+            {
+                return null;
+            }
+
+            return this.TryGetValue(key, out object value) ? value : defaultValue;
+        }
+
+        public object GetValueOrDefault(string key, Func<object> defaultValueProvider)
+        {
+            if (key.IsNullOrEmpty())
+            {
+                return null;
+            }
+
+            return this.TryGetValue(key, out object value) ? value : defaultValueProvider();
+        }
+
+        public T GetValue<T>(string key, bool throwIfKeyNotFound = false)
+        {
+            if (key.IsNullOrEmpty())
+            {
+                return default;
+            }
+
+            if (!this.ContainsKey(key) && throwIfKeyNotFound)
+            {
+                throw new KeyNotFoundException($"Key \"{key}\" not found in the dictionary.");
+            }
+
+            return this.GetValueOrDefault<T>(key);
+        }
+
+        public T GetValueOrDefault<T>(string key, T defaultValue = default)
+        {
+            if (!this.ContainsKey(key))
+            {
+                return defaultValue;
+            }
+
+            object data = this[key];
+            if (data is T)
+            {
+                return (T)data;
+            }
+
+            if (data == null)
+            {
+                return defaultValue;
+            }
+
+            try
+            {
+                return data.ToType<T>();
+            }
+            catch
+            {
+                // return default
+            }
+
+            return defaultValue;
+        }
+
+        public string GetStringValue(string name)
+        {
+            return this.GetStringValue(name, string.Empty);
+        }
+
+        public string GetStringValue(string name, string @default)
+        {
+            if (!this.TryGetValue(name, out object value))
+            {
+                return @default;
+            }
+
+            if (value is string)
+            {
+                return (string)value;
+            }
+
+            return string.Empty;
+        }
+    }
+}
