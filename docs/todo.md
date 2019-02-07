611b71d41e755385e37eae4d1b2ed823da0d3ec5
Vincent van Proosdij <mini32@gmail.com>
2019-02-07 21:54:10 +0100

    BaseOptionsBuilder

--- docs/todo.md
+++ docs/todo.md
@@ -32,6 +32,8 @@ Marketing
 
 TODO:
 
+OptionsBuilder instead of large ctors (for all non configration things like ServiceExtensions.AddXYZ)
+
 NetCore 3.0 upgrade
  https://www.talkingdotnet.com/asp-net-core-3-0-app-with-net-core-3-preview-2-and-visual-studio-2019/
 
--- src/Common/Utility/OptionsBuilder.cs
+++ src/Common/Utility/OptionsBuilder.cs
@@ -1,4 +1,6 @@
-﻿namespace Naos.Core.Common
+﻿using Microsoft.Extensions.Logging;
+
+namespace Naos.Core.Common
 {
 #pragma warning disable SA1402 // File may only contain a single class
 #pragma warning disable SA1201 // Elements must appear in the correct order
@@ -23,6 +25,22 @@
         public static T Target<T>(this IOptionsBuilder builder)
         {
             return (T)builder.Target;
+        }
+    }
+
+    public abstract class BaseOptions
+    {
+        public ILoggerFactory LoggerFactory { get; set; }
+    }
+
+    public class BaseOptionsBuilder<TOption, TBuilder> : OptionsBuilder<TOption>
+        where TOption : BaseOptions, new()
+        where TBuilder : BaseOptionsBuilder<TOption, TBuilder>
+    {
+        public TBuilder LoggerFactory(ILoggerFactory loggerFactory)
+        {
+            this.Target.LoggerFactory = loggerFactory;
+            return (TBuilder)this;
         }
     }
 #pragma warning restore SA1201 // Elements must appear in the correct order
