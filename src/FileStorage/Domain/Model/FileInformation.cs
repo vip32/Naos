2b9bdb6913b2945e306e5c2960904fd8807b7d00
Vincent van Proosdij <mini32@gmail.com>
2019-02-18 23:17:12 +0100

    ContentType for filestorage

--- src/FileStorage/Domain/Model/FileInformation.cs
+++ src/FileStorage/Domain/Model/FileInformation.cs
@@ -3,12 +3,15 @@
     using System;
     using System.Diagnostics;
     using Humanizer;
+    using Naos.Core.Common;
     using Naos.Core.Domain.Model;
 
     [DebuggerDisplay("Path = {Path}, Created = {Created}, Modified = {Modified}, Size = {Size} bytes")]
     public class FileInformation
     {
         public string Path { get; set; }
+
+        public string Name { get; set; }
 
         public DateTime Created { get; set; }
 
@@ -23,6 +26,7 @@
 
         public DataDictionary Properties => new DataDictionary();
 
-        public string Name { get; set; }
+        public ContentType ContentType =>
+            !this.Name.IsNullOrEmpty() ? ContentTypeExtensions.FromFilename(this.Name) : ContentTypeExtensions.FromFilename(this.Path);
     }
 }
