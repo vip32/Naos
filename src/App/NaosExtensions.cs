7f203199c12013d0e18e3f7210e3add78e9e0ba7
Vincent van Proosdij <mini32@gmail.com>
2019-02-26 00:29:58 +0100

    merge

--- src/App/NaosExtensions.cs
+++ src/App/NaosExtensions.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using EnsureThat;
+    using Naos.Core.Common;
     using Naos.Core.ServiceDiscovery.App;
 
     public static class NaosExtensions
@@ -26,6 +27,54 @@
             naosOptions.Context.AddServiceClient(name, setupAction);
 
             return naosOptions;
+        }
+
+        public static INaosBuilderContext AddServiceClient<TClient>(this INaosBuilderContext context, Action<IHttpClientBuilder> setupAction = null)
+            where TClient : ServiceDiscoveryClient
+        {
+            EnsureArg.IsNotNull(context, nameof(context));
+
+            context.Messages.Add($"{LogEventKeys.Startup} naos builder: typed {typeof(TClient).Name.Replace("Client", string.Empty).ToLower()} service client added (type=http)");
+
+            if (setupAction != null)
+            {
+                var builder = context.Services
+                    .AddHttpClient<TClient>();
+                setupAction.Invoke(builder);
+            }
+            else
+            {
+                context.Services
+                    .AddHttpClient<TClient>()
+                    .AddNaosMessageHandlers()
+                    .AddNaosPolicyHandlers();
+            }
+
+            return context;
+        }
+
+        public static INaosBuilderContext AddServiceClient(this INaosBuilderContext context, string name, Action<IHttpClientBuilder> setupAction = null)
+        {
+            EnsureArg.IsNotNull(context, nameof(context));
+            EnsureArg.IsNotNullOrEmpty(name, nameof(name));
+
+            context.Messages.Add($"{LogEventKeys.Startup} naos builder: named {name} service client added (type=http)");
+
+            if (setupAction != null)
+            {
+                var builder = context.Services
+                    .AddHttpClient(name);
+                setupAction.Invoke(builder);
+            }
+            else
+            {
+                context.Services
+                    .AddHttpClient(name)
+                    .AddNaosMessageHandlers()
+                    .AddNaosPolicyHandlers();
+            }
+
+            return context;
         }
     }
 }
