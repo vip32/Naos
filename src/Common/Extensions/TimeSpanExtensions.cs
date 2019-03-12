81acd0e7ca6e9cf989d3c3ec88c4295bda792c0d
Vincent van Proosdij <mini32@gmail.com>
2019-03-12 11:00:22 +0100

    ext

---
+++ src/Common/Extensions/TimeSpanExtensions.cs
@@ -0,0 +1,50 @@
+﻿namespace Naos.Core.Common
+{
+    using System;
+    using System.Threading;
+
+    public static class TimeSpanExtensions
+    {
+        public static CancellationTokenSource ToCancellationTokenSource(this TimeSpan source)
+        {
+            if (source == TimeSpan.Zero)
+            {
+                var result = new CancellationTokenSource();
+                result.Cancel();
+                return result;
+            }
+
+            if (source.Ticks > 0)
+            {
+                return new CancellationTokenSource(source);
+            }
+
+            return new CancellationTokenSource();
+        }
+
+        public static CancellationTokenSource ToCancellationTokenSource(this TimeSpan? source)
+        {
+            if (source.HasValue)
+            {
+                return source.Value.ToCancellationTokenSource();
+            }
+
+            return new CancellationTokenSource();
+        }
+
+        public static CancellationTokenSource ToCancellationTokenSource(this TimeSpan? source, TimeSpan defaultTimeout)
+        {
+            return (source ?? defaultTimeout).ToCancellationTokenSource();
+        }
+
+        public static TimeSpan Min(this TimeSpan source, TimeSpan other)
+        {
+            return source.Ticks > other.Ticks ? other : source;
+        }
+
+        public static TimeSpan Max(this TimeSpan source, TimeSpan other)
+        {
+            return source.Ticks < other.Ticks ? other : source;
+        }
+    }
+}
