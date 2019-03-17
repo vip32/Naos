e5e41931296d8c6e6b0807f4d202633f286e1976
Vincent van Proosdij <mini32@gmail.com>
2019-03-17 23:22:40 +0100

    QueueProcessHostedService

---
+++ src/Queueing.App/QueueProcessHostedService.cs
@@ -0,0 +1,53 @@
+﻿namespace Queueing.App
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using EnsureThat;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+    using Naos.Core.Queueing.Domain;
+
+    public class QueueProcessHostedService<T> : IHostedService, IDisposable
+        where T : class
+    {
+        private readonly ILogger<QueueProcessHostedService<T>> logger;
+        private readonly IQueue<T> queue;
+        private readonly Func<IQueueItem<T>, Task> handler;
+
+        public QueueProcessHostedService(
+            ILogger<QueueProcessHostedService<T>> logger,
+            IQueue<T> queue,
+            Func<IQueueItem<T>, Task> handler = null)
+        {
+            EnsureArg.IsNotNull(logger, nameof(logger));
+            EnsureArg.IsNotNull(queue, nameof(queue));
+
+            this.logger = logger;
+            this.queue = queue;
+            this.handler = handler;
+            //handler = i => { i.CompleteAsync()};
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            //await this.queue.ProcessItemsAsync(this.handler);
+            await this.queue.ProcessItemsAsync(async i =>
+            {
+                // do something;
+                this.logger.LogInformation("+++ process item +++");
+                await i.CompleteAsync();
+            });
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            this.queue?.Dispose();
+        }
+    }
+}
