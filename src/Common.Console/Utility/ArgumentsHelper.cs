fc3ef2e7d8edf5df06586d725c29538a0b63c9d9
Vincent van Proosdij <mini32@gmail.com>
2019-03-27 21:56:33 +0100

    added common.console + ArgumentsHelper

---
+++ src/Common.Console/Utility/ArgumentsHelper.cs
@@ -0,0 +1,68 @@
+﻿namespace Naos.Core.Common.Console
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public static class ArgumentsHelper
+    {
+        public static string[] Split(string line)
+        {
+            if (line.IsNullOrEmpty())
+            {
+                return Enumerable.Empty<string>().ToArray();
+            }
+
+            var result = new List<string>();
+            var currentArgument = new StringBuilder();
+            char currentQuote = char.MinValue;
+
+            Action reset = () =>
+            {
+                result.Add(currentArgument.ToString());
+                currentArgument = new StringBuilder();
+                currentQuote = char.MinValue;
+            };
+
+            foreach (char c in line)
+            {
+                if (currentQuote == char.MinValue)
+                {
+                    if (c == ' ')
+                    {
+                        reset();
+                    }
+                    else if (c == '\'')
+                    {
+                        reset();
+                        currentQuote = '\'';
+                    }
+                    else if (c == '"')
+                    {
+                        reset();
+                        currentQuote = '"';
+                    }
+                    else
+                    {
+                        currentArgument.Append(c);
+                    }
+                }
+                else
+                {
+                    if (c == currentQuote)
+                    {
+                        reset();
+                    }
+                    else
+                    {
+                        currentArgument.Append(c);
+                    }
+                }
+            }
+
+            reset();
+            return result.Where(a => !string.IsNullOrWhiteSpace(a)).ToArray();
+        }
+    }
+}
---
+++ tests/UnitTests/Common.Console/Utility/ArgumentsHelperTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Naos.Core.UnitTests.Common.Console
+{
+    using Naos.Core.Common.Console;
+    using Shouldly;
+    using Xunit;
+
+    public class ArgumentsHelperTests
+    {
+        [Fact]
+        public void CanParseLine_Test()
+        {
+            var args = ArgumentsHelper.Split("git commit -m 'message 123'");
+
+            args.ShouldContain("git");
+            args.ShouldContain("commit");
+            args.ShouldContain("-m");
+            args.ShouldContain("message 123");
+        }
+    }
+}
