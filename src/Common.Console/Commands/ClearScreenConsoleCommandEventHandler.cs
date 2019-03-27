a42281a3a76a00bb2f065153772d4be4a09912df
Vincent van Proosdij <mini32@gmail.com>
2019-03-27 23:00:59 +0100

    ClearScreenConsoleCommand + EventHandler

---
+++ src/Common.Console/Commands/ClearScreenConsoleCommandEventHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Naos.Core.Common.Console
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class ClearScreenConsoleCommandEventHandler : ConsoleCommandEventHandler<ClearScreenConsoleCommand>
+    {
+        public override async Task<bool> Handle(ConsoleCommandEvent<ClearScreenConsoleCommand> request, CancellationToken cancellationToken)
+        {
+            await Task.Run(() => System.Console.Clear());
+            return true;
+        }
+    }
+}
--- src/Common.Console/Commands/EchoConsoleCommandEventHandler.cs
+++ src/Common.Console/Commands/EchoConsoleCommandEventHandler.cs
@@ -2,7 +2,6 @@
 {
     using System.Threading;
     using System.Threading.Tasks;
-    using MediatR;
     using Microsoft.Extensions.Logging;
 
     public class EchoConsoleCommandEventHandler : ConsoleCommandEventHandler<EchoConsoleCommand>
