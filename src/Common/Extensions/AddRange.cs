6ecd742858e850cd5afd04470cbe77932ea47556
Vincent van Proosdij <mini32@gmail.com>
2019-04-01 21:06:14 +0200

    extensions

---
+++ src/Common/Extensions/AddRange.cs
@@ -0,0 +1,20 @@
+﻿namespace Naos.Core.Common
+{
+    using System.Collections.Generic;
+
+    public static partial class Extensions
+    {
+        public static void AddRange<T>(this ICollection<T> source, IEnumerable<T> items)
+        {
+            if (source == null || items == null)
+            {
+                return;
+            }
+
+            foreach (T item in items)
+            {
+                source.Add(item);
+            }
+        }
+    }
+}
---
+++ src/Common/Extensions/Chunk.cs
@@ -0,0 +1,29 @@
+﻿namespace Naos.Core.Common
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// Split sequence in batches of specified size
+        /// </summary>
+        /// <typeparam name="T">Element type</typeparam>
+        /// <param name="source">Enumeration source</param>
+        /// <param name="size">Size of the batch chunk</param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> source, int size)
+        {
+            if (source.IsNullOrEmpty())
+            {
+                yield return null;
+            }
+
+            while (source.Any())
+            {
+                yield return source.Take(size);
+                source = source.Skip(size);
+            }
+        }
+    }
+}
