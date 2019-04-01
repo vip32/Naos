ed3db0a632843ac2dc3ee23e4d46d50791935b56
Vincent van Proosdij <mini32@gmail.com>
2019-04-02 00:56:01 +0200

    csvserializer body

---
+++ src/Common.Serialization/Serializers/CSvSerializer.cs
@@ -0,0 +1,26 @@
+﻿namespace Naos.Core.Common.Serialization
+{
+    using System;
+    using System.IO;
+
+    public class CsvSerializer : ITextSerializer
+    {
+        public object Deserialize(Stream input, Type type)
+        {
+            // TODO: https://github.com/ServiceStack/ServiceStack.Text/blob/master/src/ServiceStack.Text/CsvSerializer.cs
+            throw new NotImplementedException();
+        }
+
+        public T Deserialize<T>(Stream input)
+        {
+            // TODO: https://github.com/ServiceStack/ServiceStack.Text/blob/master/src/ServiceStack.Text/CsvSerializer.cs
+            throw new NotImplementedException();
+        }
+
+        public void Serialize(object value, Stream output)
+        {
+            // TODO: https://github.com/ServiceStack/ServiceStack.Text/blob/master/src/ServiceStack.Text/CsvSerializer.cs
+            throw new NotImplementedException();
+        }
+    }
+}
