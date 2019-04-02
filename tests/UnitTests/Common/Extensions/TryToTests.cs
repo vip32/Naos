9fc49af85adc58f6a3bd9c0a1d2d25994dbbbed9
Vincent van Proosdij <mini32@gmail.com>
2019-04-02 21:41:31 +0200

    tests

--- tests/UnitTests/Common/Extensions/TryToTests.cs
+++ tests/UnitTests/Common/Extensions/TryToTests.cs
@@ -1,6 +1,5 @@
 ﻿namespace Naos.Core.UnitTests.Common
 {
-    using System;
     using Naos.Core.Common;
     using Shouldly;
     using Xunit;
@@ -13,11 +12,17 @@
             const string s = null;
             s.TryTo(out int _).ShouldBe(false);
             "42".TryTo(out int _).ShouldBe(true);
+            "42".TryTo(out decimal _).ShouldBe(true); // also decimal, not pure int
             "28173829281734".TryTo(out long _).ShouldBe(true);
+            "28173829281734".TryTo(out int _).ShouldBe(false);
             "2.0".TryTo(out double _).ShouldBe(true);
             2.TryTo(out int _).ShouldBe(true);
+            2.TryTo(out decimal _).ShouldBe(true); // also decimal, not pure int
+            2.TryTo(out double _).ShouldBe(true); // also double, not pure int
             "0.2".TryTo(out double _).ShouldBe(true);
+            "0.2".TryTo(out int _).ShouldBe(false);
             2.0.TryTo(out double _).ShouldBe(true);
+            2.2.TryTo(out int _).ShouldBe(true); // also int, not pure double (rounded!)
         }
     }
 }
