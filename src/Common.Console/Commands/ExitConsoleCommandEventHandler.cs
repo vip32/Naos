4b2436bab2ba871ab2a9fd2b1588602209269b81
Vincent van Proosdij <mini32@gmail.com>
2019-04-04 20:56:53 +0200

    save console history

--- src/Common.Console/Commands/ExitConsoleCommandEventHandler.cs
+++ src/Common.Console/Commands/ExitConsoleCommandEventHandler.cs
@@ -12,23 +12,34 @@
     {
         public override Task<bool> Handle(ConsoleCommandEvent<ExitConsoleCommand> request, CancellationToken cancellationToken)
         {
+            this.SaveHistory();
+
+            Environment.Exit((int)ExitCode.Termination);
+
+            return Task.FromResult(true);
+        }
+
+        private void SaveHistory()
+        {
             // save history for later use
+            var directory = Path.Combine(Path.GetTempPath(), "naos_console");
+            if(!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
             using(var writer = new StreamWriter(
-                File.Create(Path.Combine(Path.GetTempPath(), "naos_console", "history.db"))))
+                File.Create(Path.Combine(directory, "history.db"))))
             {
                 Console.WriteLine("saving history", Color.Gray);
                 foreach(var history in ReadLine.GetHistory().Distinct())
                 {
-                    if(!history.IsNullOrEmpty() && !history.EqualsAny(new[] { "exit"}))
+                    if(!history.IsNullOrEmpty() && !history.EqualsAny(new[] { "exit" }))
                     {
                         writer.WriteLine(history);
                     }
                 }
             }
-
-            Environment.Exit((int)ExitCode.Termination);
-
-            return Task.FromResult(true);
         }
     }
 }
