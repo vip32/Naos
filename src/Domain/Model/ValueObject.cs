3edecc5601ff8766cebcc8c3a00b8f2db7c9811b
Vincent van Proosdij <mini32@gmail.com>
2019-05-02 20:45:52 +0200

    ValueObject + tests

--- src/Domain/Model/ValueObject.cs
+++ src/Domain/Model/ValueObject.cs
@@ -3,9 +3,15 @@
     using System.Collections.Generic;
     using System.Linq;
 
-    // Source: https://docs.microsoft.com/en-us/dotnet/standard/microservices-architecture/microservice-ddd-cqrs-patterns/implement-value-objects
+    /// <summary>
+    /// A Value Object is an immutable type that is distinguishable only by the state of
+    /// its properties. That is, unlike an Entity, which has a unique identifier and remains
+    /// distinct even if its properties are otherwise identical, two Value Objects with the
+    /// exact same properties can be considered equal.
+    /// </summary>
     public abstract class ValueObject
     {
+        // Source: https://docs.microsoft.com/en-us/dotnet/standard/microservices-architecture/microservice-ddd-cqrs-patterns/implement-value-objects
         public override bool Equals(object obj)
         {
             if(obj == null || obj.GetType() != this.GetType())
@@ -24,8 +30,7 @@
                     return false;
                 }
 
-                if(thisValues.Current != null &&
-                    !thisValues.Current.Equals(otherValues.Current))
+                if(thisValues.Current?.Equals(otherValues.Current) == false)
                 {
                     return false;
                 }
@@ -37,7 +42,7 @@
         public override int GetHashCode()
         {
             return this.GetAtomicValues()
-                .Select(x => x != null ? x.GetHashCode() : 0)
+                .Select(x => x?.GetHashCode() ?? 0)
                 .Aggregate((x, y) => x ^ y);
         }
 
@@ -59,72 +64,3 @@
         protected abstract IEnumerable<object> GetAtomicValues();
     }
 }
-
-
-//namespace Naos.Core.Domain
-//{
-//    using System;
-
-//    public abstract class ValueObject
-//    {
-//        /// <summary>
-//        /// Implements the operator ==.
-//        /// </summary>
-//        /// <param name="a">The first object instance</param>
-//        /// <param name="b">The second object instance</param>
-//        /// <returns>
-//        /// The result of the operator.
-//        /// </returns>
-//        public static bool operator ==(ValueObject a, ValueObject b)
-//        {
-//            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
-//            {
-//                return true;
-//            }
-
-//            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
-//            {
-//                return false;
-//            }
-
-//            return a.Equals(b);
-//        }
-
-//        /// <summary>
-//        /// Implements the operator !=.
-//        /// </summary>
-//        /// <param name="a">The first object instance</param>
-//        /// <param name="b">The second object instance</param>
-//        /// <returns>
-//        /// The result of the operator.
-//        /// </returns>
-//        public static bool operator !=(ValueObject a, ValueObject b) => !(a == b);
-
-//        /// <summary>
-//        ///     Determines whether the specified <see cref="Object" /> is equal to this instance.
-//        /// </summary>
-//        /// <param name="obj">The <see cref="Object" /> to compare with the current <see cref="Object" />.</param>
-//        /// <returns><c>true</c> if the specified <see cref="Object" /> is equal to this instance; otherwise, <c>false</c>.</returns>
-//        public override bool Equals(object obj)
-//        {
-//            return base.Equals(obj);
-//        }
-
-//        /// <summary>
-//        ///     Returns a hash code for this instance.
-//        /// </summary>
-//        /// <returns>A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.</returns>
-//        /// <remarks>
-//        ///     This is used to provide the hash code identifier of an object using the signature
-//        ///     properties of the object; although it's necessary for NHibernate's use, this can
-//        ///     also be useful for business logic purposes and has been included in this base
-//        ///     class, accordingly. Since it is recommended that GetHashCode change infrequently,
-//        ///     if at all, in an object's lifetime, it's important that properties are carefully
-//        ///     selected which truly represent the signature of an object.
-//        /// </remarks>
-//        public override int GetHashCode()
-//        {
-//            return base.GetHashCode();
-//        }
-//    }
-//}
---
+++ tests/UnitTests/Domain/Model/ValueObjectTests.cs
@@ -0,0 +1,47 @@
+﻿namespace Naos.Core.UnitTests.Domain.Model
+{
+    using System.Collections.Generic;
+    using Naos.Core.Domain;
+    using Shouldly;
+    using Xunit;
+
+    public class ValueObjectTests
+    {
+        [Fact]
+        public void EqualityTests()
+        {
+            var instance1 = new StubValueObject
+            {
+                StreetName = "One Microsoft Way",
+                HouseNumber = 1,
+                City = "Seattle"
+            };
+
+            var instance2 = new StubValueObject
+            {
+                StreetName = "One Microsoft Way",
+                HouseNumber = 1,
+                City = "New York"
+            };
+
+            instance1.ShouldBe(instance1);
+            instance1.ShouldNotBe(instance2);
+        }
+
+        public class StubValueObject : ValueObject
+        {
+            public string StreetName { get; set; }
+
+            public int HouseNumber { get; set; }
+
+            public string City { get; set; }
+
+            protected override IEnumerable<object> GetAtomicValues()
+            {
+                yield return this.StreetName;
+                yield return this.HouseNumber;
+                yield return this.City;
+            }
+        }
+    }
+}
