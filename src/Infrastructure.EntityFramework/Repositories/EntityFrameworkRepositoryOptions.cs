57121e9300e642cde2546fd6e82755a859090b85
Vincent van Proosdij <mini32@gmail.com>
2019-05-04 22:04:52 +0200

    doc

--- src/Infrastructure.EntityFramework/Repositories/EntityFrameworkRepositoryOptions.cs
+++ src/Infrastructure.EntityFramework/Repositories/EntityFrameworkRepositoryOptions.cs
@@ -6,10 +6,22 @@
 
     public class EntityFrameworkRepositoryOptions : BaseOptions
     {
+        /// <summary>
+        /// Gets or sets the mediator.
+        /// </summary>
+        /// <value>
+        /// The mediator.
+        /// </value>
         public IMediator Mediator { get; set; }
 
         public bool PublishEvents { get; set; } = true; // Obsolete > optional decorator
 
+        /// <summary>
+        /// Gets or sets the database context.
+        /// </summary>
+        /// <value>
+        /// The database context.
+        /// </value>
         public DbContext DbContext { get; set; }
     }
 }
