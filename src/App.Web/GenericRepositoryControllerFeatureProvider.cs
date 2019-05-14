c4b17ca8e3655cf5fa40756af4db10377c172afa
Vincent van Proosdij <mini32@gmail.com>
2019-05-14 22:10:54 +0200

    docs

--- src/App.Web/GenericRepositoryControllerFeatureProvider.cs
+++ src/App.Web/GenericRepositoryControllerFeatureProvider.cs
@@ -12,12 +12,26 @@
     {
         private readonly IEnumerable<GenericRepositoryControllerInformation> informations;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenericRepositoryControllerFeatureProvider"/> class.
+        /// </summary>
+        /// <param name="informations">The informations.</param>
         public GenericRepositoryControllerFeatureProvider(
             IEnumerable<GenericRepositoryControllerInformation> informations)
         {
             this.informations = informations;
         }
 
+        /// <summary>
+        /// Updates the <paramref name="feature" /> instance.
+        /// </summary>
+        /// <param name="parts">The list of <see cref="T:Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPart" /> instances in the application.</param>
+        /// <param name="feature">The feature instance to populate.</param>
+        /// <remarks>
+        /// <see cref="T:Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPart" /> instances in <paramref name="parts" /> appear in the same ordered sequence they
+        /// are stored in <see cref="P:Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartManager.ApplicationParts" />. This ordering may be used by the feature
+        /// provider to make precedence decisions.
+        /// </remarks>
         public void PopulateFeature(IEnumerable<ApplicationPart> parts, ControllerFeature feature)
         {
             // https://www.strathweb.com/2018/04/generic-and-dynamically-generated-controllers-in-asp-net-core-mvc/
