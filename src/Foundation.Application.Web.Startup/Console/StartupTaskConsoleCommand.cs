f2e536b733cb7986dde01be7e4d2895f33e1d335
Vincent van Proosdij <mini32@gmail.com>
2019-06-28 00:41:43 +0200

    startup tasks console command

---
+++ src/Foundation.Application.Web.Startup/Console/StartupTaskConsoleCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace Naos.Foundation.Application
+{
+    using System.Threading.Tasks;
+    using CommandLine;
+    using MediatR;
+
+    [Verb("startuptasks", HelpText = "Startup tasks information")]
+    public class StartupTaskConsoleCommand : IConsoleCommand
+    {
+        [Option('l', "list", HelpText = "Shows all registered startup tasks", Required = true)]
+        public bool List { get; set; }
+
+        public async Task<bool> SendAsync(IMediator mediator)
+        {
+            return await mediator.Send(new ConsoleCommandEvent<StartupTaskConsoleCommand>(this)).AnyContext();
+        }
+    }
+}
---
+++ src/Foundation.Application.Web.Startup/Console/StartupTaskConsoleCommandEventHandler.cs
@@ -0,0 +1,34 @@
+﻿namespace Naos.Foundation.Application
+{
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Logging;
+    using Console = Colorful.Console;
+
+    public class StartupTaskConsoleCommandEventHandler : ConsoleCommandEventHandler<StartupTaskConsoleCommand>
+    {
+        private readonly ILogger<StartupTaskConsoleCommandEventHandler> logger;
+        private readonly IEnumerable<IStartupTask> tasks;
+
+        public StartupTaskConsoleCommandEventHandler(ILogger<StartupTaskConsoleCommandEventHandler> logger, IEnumerable<IStartupTask> tasks)
+        {
+            this.logger = logger;
+            this.tasks = tasks;
+        }
+
+        public override Task<bool> Handle(ConsoleCommandEvent<StartupTaskConsoleCommand> request, CancellationToken cancellationToken)
+        {
+            if(request.Command.List)
+            {
+                foreach(var task in this.tasks.Safe())
+                {
+                    Console.WriteLine($"- {task.GetType().PrettyName()}", Color.Gray);
+                }
+            }
+
+            return Task.FromResult(true);
+        }
+    }
+}
