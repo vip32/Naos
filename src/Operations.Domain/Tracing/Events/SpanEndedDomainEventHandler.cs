2de75ac84db91275ff3e69e52cf1b3a8ec9efc69
Vincent van Proosdij <mini32@gmail.com>
2019-07-02 17:02:10 +0200

    tracing event handler

---
+++ src/Operations.Domain/Tracing/Events/SpanEndedDomainEventHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace Naos.Core.Operations.Domain
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Logging;
+    using Naos.Foundation.Domain;
+
+    public class SpanEndedDomainEventHandler
+        : IDomainEventHandler<SpanEndedDomainEvent>
+    {
+        private readonly ILogger<SpanEndedDomainEventHandler> logger;
+
+        public SpanEndedDomainEventHandler(ILogger<SpanEndedDomainEventHandler> logger)
+        {
+            this.logger = logger;
+        }
+
+        public bool CanHandle(SpanEndedDomainEvent notification)
+        {
+            return notification?.Span != null;
+        }
+
+        public Task Handle(SpanEndedDomainEvent notification, CancellationToken cancellationToken)
+        {
+            this.logger.LogInformation($"{{LogKey:l}} TRACING!!!!!!!!!!! {notification.Span.OperationName} {notification.Span.SpanId}");
+            return Task.CompletedTask;
+        }
+    }
+}
