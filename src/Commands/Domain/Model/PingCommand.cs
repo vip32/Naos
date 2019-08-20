51f5822251ff3753a4e8d4b412460a010c354bbd
Vincent van Proosdij <mini32@gmail.com>
2019-08-21 01:27:27 +0200

    PingCommand

--- src/Commands/Domain/Model/PingCommand.cs
+++ src/Commands/Domain/Model/PingCommand.cs
@@ -1,12 +1,6 @@
 ﻿namespace Naos.Core.Commands.Domain
 {
-    using FluentValidation.Results;
-
     public class PingCommand : CommandRequest<object> // has no response type
     {
-        public string Message { get; set; }
-
-        public override ValidationResult Validate() =>
-            new PingCommandValidator().Validate(this);
     }
 }
