d8e0185002900122625a17b6e773f61a2c54dbea
Vincent van Proosdij <mini32@gmail.com>
2019-08-21 10:57:53 +0200

    request command POC + body model

--- src/Commands.App/Web/RequestCommandDocumentProcessor.cs
+++ src/Commands.App/Web/RequestCommandDocumentProcessor.cs
@@ -1,6 +1,7 @@
 ﻿namespace Naos.Core.App.Web
 {
     using System.Linq;
+    using NSwag;
     using NSwag.Generation.Processors;
     using NSwag.Generation.Processors.Contexts;
 
@@ -9,20 +10,28 @@
         public void Process(DocumentProcessorContext context)
         {
             // TODO: foreach RequestCommandRegistration add the following swagger path
-            var item = new NSwag.OpenApiPathItem
+            var item = new OpenApiPathItem
             {
                 {
-                    "get",
-                    new NSwag.OpenApiOperation
+                    "post",
+                    new OpenApiOperation
                     {
                         OperationId = "command1",
                         Summary = "test operation",
                         Description = "test operation long",
                         Tags = new[] { "Naos Commands" }.ToList(),
                         Produces = new[] { "application/json" }.ToList(),
+                        //RequestBody = new OpenApiRequestBody{}
                     }
                 }
             };
+
+            item.Values.FirstOrDefault()?.Parameters.Add(new OpenApiParameter
+            {
+                Kind = OpenApiParameterKind.Body,
+                Name = "model",
+                Type = NJsonSchema.JsonObjectType.Object,
+            });
 
             context.Document.Paths.Add("/api/test", item);
         }
