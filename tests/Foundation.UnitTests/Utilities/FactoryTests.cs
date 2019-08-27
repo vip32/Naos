16e413d8936e110699038752452c74702a90ff17
Vincent van Proosdij <mini32@gmail.com>
2019-08-27 09:36:00 +0200

    factory.create with dictionary props

--- tests/Foundation.UnitTests/Utilities/FactoryTests.cs
+++ tests/Foundation.UnitTests/Utilities/FactoryTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace Naos.Foundation.UnitTests.Utilities
 {
+    using System.Collections.Generic;
     using Naos.Foundation;
     using Shouldly;
     using Xunit;
@@ -10,12 +11,47 @@
         public void CanCreateInstance()
         {
             Factory<Stub>.Create().ShouldNotBeNull();
-            Factory<Stub>.Create(typeof(Stub)).ShouldNotBeNull();
-            Factory<Stub>.Create(typeof(Stub), "firstname").ShouldNotBeNull();
-            Factory<Stub>.Create(typeof(Stub), "firstname", "NOARG").ShouldBeNull();
+            Factory<Stub>.Create("firstname").ShouldNotBeNull();
+            Factory<Stub>.Create("firstname", "NOARG").ShouldBeNull();
             Factory.Create(typeof(Stub)).ShouldNotBeNull();
             Factory.Create(typeof(Stub), "firstname").ShouldNotBeNull();
             Factory.Create(typeof(Stub), "firstname", "NOARG").ShouldBeNull();
+        }
+
+        [Fact]
+        public void CanCreateWithDictionaryProperties1Instance()
+        {
+            // arrange
+            var properties = new Dictionary<string, object>
+            {
+                ["Firstname"] = "John",
+                ["lastname"] = "Doe"
+            };
+
+            // act
+            var sut = Factory<Stub>.Create(properties);
+
+            // assert
+            sut.FirstName.ShouldBe("John");
+            sut.LastName.ShouldBe("Doe");
+        }
+
+        [Fact]
+        public void CanCreateWithDictionaryProperties2Instance()
+        {
+            // arrange
+            var properties = new Dictionary<string, object>
+            {
+                ["Firstname"] = "John",
+                ["lastname"] = "Doe"
+            };
+
+            // act
+            var sut = Factory.Create(typeof(Stub), properties) as Stub;
+
+            // assert
+            sut.FirstName.ShouldBe("John");
+            sut.LastName.ShouldBe("Doe");
         }
 
         public class Stub
