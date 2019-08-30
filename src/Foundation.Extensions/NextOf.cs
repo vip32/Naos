2b087919dc062a2ea4a47c34032c5782c10e517b
Vincent van Proosdij <mini32@gmail.com>
2019-08-30 20:54:27 +0200

    list next/prev extensions

---
+++ src/Foundation.Extensions/NextOf.cs
@@ -0,0 +1,23 @@
+﻿namespace Naos.Foundation
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static partial class Extensions
+    {
+        public static T NextOf<T>(this List<T> source, T item)
+        {
+            if (source.IsNullOrEmpty())
+            {
+                return default;
+            }
+
+            if(source.LastOrDefault().Equals(item)) // stop at last item
+            {
+                return default;
+            }
+
+            return source.SkipWhile(x => !x.Equals(item)).Skip(1).DefaultIfEmpty(source[0]).FirstOrDefault();
+        }
+    }
+}
---
+++ src/Foundation.Extensions/PreviousOf.cs
@@ -0,0 +1,23 @@
+﻿namespace Naos.Foundation
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static partial class Extensions
+    {
+        public static T PreviousOf<T>(this List<T> source, T item)
+        {
+            if (source.IsNullOrEmpty())
+            {
+                return default;
+            }
+
+            if (source.FirstOrDefault().Equals(item)) // stop at first item
+            {
+                return default;
+            }
+
+            return source.TakeWhile(x => !x.Equals(item)).DefaultIfEmpty(source[source.Count - 1]).LastOrDefault();
+        }
+    }
+}
