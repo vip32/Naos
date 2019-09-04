d43e302cda7b41f0b2e16ae7e8c7a7753ade86d7
Vincent van Proosdij <mini32@gmail.com>
2019-09-04 21:29:46 +0200

    +RouteMatcher

---
+++ src/Utilities.Web/RouteMatcher.cs
@@ -0,0 +1,55 @@
+﻿namespace Naos.Foundation
+{
+    //using System.Linq;
+    using System.Text.RegularExpressions;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Routing;
+    using Microsoft.AspNetCore.Routing.Template;
+    using Microsoft.AspNetCore.WebUtilities;
+
+    public class RouteMatcher
+    {
+        public RouteValueDictionary Match(string routeTemplate, string requestPath, IQueryCollection query = null)
+        {
+            // The TemplateParser can only parse the route part, and not the query string.
+            // If the template provided by the user also has a query string, we separate that and match it manually.
+            var regex = new Regex(@"(.*)(\?[^{}]*$)");
+            var match = regex.Match(routeTemplate);
+            if (match.Success)
+            {
+                var queryString = match.Groups[2].Value;
+                routeTemplate = match.Groups[1].Value;
+                var queryInTemplate = QueryHelpers.ParseQuery(queryString);
+
+                //if (!query.All(arg => queryInTemplate.ContainsKey(arg.Key.TrimStart('?')) && queryInTemplate[arg.Key.TrimStart('?')] == arg.Value))
+                //{
+                //    return null;
+                //}
+            }
+
+            var template = TemplateParser.Parse(routeTemplate);
+            var matcher = new TemplateMatcher(template, this.GetDefaults(template));
+            var values = new RouteValueDictionary();
+
+            return matcher.TryMatch(requestPath, values) ? values : null;
+        }
+
+        private RouteValueDictionary GetDefaults(RouteTemplate parsedTemplate)
+        {
+            var result = new RouteValueDictionary();
+
+            if (parsedTemplate.Parameters != null)
+            {
+                foreach (var parameter in parsedTemplate.Parameters)
+                {
+                    if (parameter.DefaultValue != null)
+                    {
+                        result.Add(parameter.Name, parameter.DefaultValue);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ tests/Foundation.UnitTests/Utilities.Web/RouteMatcherTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Naos.Foundation.UnitTests.Utilities
+{
+    using Naos.Foundation;
+    using Shouldly;
+    using Xunit;
+
+    public class RouteMatcherTests
+    {
+        [Fact]
+        public void VariousMatches()
+        {
+            // arrange
+            var sut = new RouteMatcher();
+
+            // act/assert
+            sut.Match("/api/customers", "/api/customers").ShouldNotBeNull();
+            sut.Match("/api/customers", "/api/customers/1234567").ShouldBeNull();
+            sut.Match("/api/customers/{id}", "/api/customers/1234567").ShouldNotBeNull();
+            sut.Match("/api/customers/{id}", "/api/customers/1234567").ContainsKey("id").ShouldBeTrue();
+            sut.Match("/api/customers/{id}", "/api/customers/1234567")["id"].ShouldBe("1234567");
+            sut.Match("/api/customers/{id}", "/api/customers/1234567/orders").ShouldBeNull();
+        }
+    }
+}
