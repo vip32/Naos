0548354a113bc8b98f943e4fb27e00b6ed22f0d0
Vincent van Proosdij <mini32@gmail.com>
2019-09-13 12:49:17 +0200

    OBJECT > STREAM > COMPRESS > FILE testcase

--- src/Foundation.Compression/FileCompressionHelper.cs
+++ src/Foundation.Compression/FileCompressionHelper.cs
@@ -6,6 +6,26 @@
     public static class FileCompressionHelper
     {
         /// <summary>
+        /// Compress the stream to a file (Gzip).
+        /// </summary>
+        /// <param name="input">the stream to compress</param>
+        /// <param name="destinationPath">Path to the compressed file.</param>
+        public static void Compress(Stream input, string destinationPath)
+        {
+            EnsureArg.IsNotNullOrEmpty(destinationPath, nameof(destinationPath));
+
+            using (var destination = File.Create(destinationPath))
+            {
+                if (input != null)
+                {
+                    input.Position = 0;
+                    input.Compress(destination);
+                    destination.Flush();
+                }
+            }
+        }
+
+        /// <summary>
         /// Compress the file (Gzip).
         /// </summary>
         /// <param name="sourcePath">Path to the file to compress.</param>
@@ -22,6 +42,25 @@
                 using (var destination = File.Create(destinationPath))
                 {
                     source.Compress(destination);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Decompress the file (Gzip).
+        /// </summary>
+        /// <param name="sourcePath">Path to the file to decompress.</param>
+        public static void Decompress(string sourcePath, Stream output)
+        {
+            EnsureArg.IsNotNullOrEmpty(sourcePath, nameof(sourcePath));
+            EnsureArg.IsTrue(File.Exists(sourcePath), nameof(sourcePath)); // source file does not exist
+
+            using (var source = File.OpenRead(sourcePath))
+            {
+                if (output != null)
+                {
+                    source.Decompress(output);
+                    output.Position = 0;
                 }
             }
         }
--- tests/Foundation.UnitTests/Compression/FileCompressionHelperTests.cs
+++ tests/Foundation.UnitTests/Compression/FileCompressionHelperTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace Naos.Foundation.UnitTests.Compression
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
     using Naos.Foundation;
     using Shouldly;
@@ -9,7 +10,7 @@
     public class FileCompressionHelperTests
     {
         [Fact]
-        public void CompressionRoundtrip_Test()
+        public void FileCompressionRoundtrip_Test()
         {
             // arrange, write test file
             var path = Path.Combine($"test-{Guid.NewGuid().ToString("N").Substring(10)}");
@@ -27,5 +28,48 @@
             // assert
             File.ReadAllLines(path + ".new.txt")[0].ShouldBe(content);
         }
+
+        [Fact]
+        public void StreamCompressionRoundtrip_Test()
+        {
+            // arrange, write test file
+            var serializer = new JsonNetSerializer();
+            var path = Path.Combine($"test-{Guid.NewGuid().ToString("N").Substring(10)}.gz");
+            var model = new StubModel
+            {
+                StringProperty = "abc"
+            };
+            StubModel newModel = null;
+
+            // act
+            using (var input = new MemoryStream())
+            {
+                serializer.Serialize(model, input); // to stream
+                FileCompressionHelper.Compress(input, path); // from stream
+            }
+
+            using (var output = new MemoryStream())
+            {
+                FileCompressionHelper.Decompress(path, output); // to stream
+                newModel = serializer.Deserialize<StubModel>(output); // from stream
+            }
+
+            // assert
+            newModel.ShouldNotBeNull();
+            newModel.StringProperty.ShouldBe(model.StringProperty);
+        }
+    }
+
+#pragma warning disable SA1402 // File may only contain a single class
+    public class StubModel
+#pragma warning restore SA1402 // File may only contain a single class
+    {
+        public int IntProperty { get; set; }
+
+        public string StringProperty { get; set; }
+
+        public List<int> ListProperty { get; set; }
+
+        public object ObjectProperty { get; set; }
     }
 }
