b4e961ae7f1b26d11bd566be5d7a4940e1e6a42d
Vincent van Proosdij <mini32@gmail.com>
2019-10-02 19:21:24 +0200

    foundation upgrade

--- src/Foundation.Application.Web/Extensions/MvcJsonOptionsExtensions.cs
+++ src/Foundation.Application.Web/Extensions/MvcJsonOptionsExtensions.cs
@@ -1,23 +1,23 @@
-﻿namespace Naos.Foundation.Application
-{
-    using Microsoft.AspNetCore.Mvc;
-    using Newtonsoft.Json;
+﻿//namespace Naos.Foundation.Application
+//{
+//    using Microsoft.AspNetCore.Mvc;
+//    using Newtonsoft.Json;
 
-    public static class MvcJsonOptionsExtensions
-    {
-        public static MvcJsonOptions AddDefaultJsonSerializerSettings(
-            this MvcJsonOptions source,
-            JsonSerializerSettings settings = null)
-        {
-            settings ??= DefaultJsonSerializerSettings.Create();
+//    public static class MvcJsonOptionsExtensions
+//    {
+//        public static MvcJsonOptions AddDefaultJsonSerializerSettings(
+//            this MvcJsonOptions source,
+//            JsonSerializerSettings settings = null)
+//        {
+//            settings ??= DefaultJsonSerializerSettings.Create();
 
-            // copy some json serializer settings for the mvcoptions
-            source.SerializerSettings.ContractResolver = settings.ContractResolver;
-            source.SerializerSettings.Converters = settings.Converters;
-            source.SerializerSettings.DefaultValueHandling = settings.DefaultValueHandling;
-            source.SerializerSettings.NullValueHandling = settings.NullValueHandling;
+//            // copy some json serializer settings for the mvcoptions
+//            source.SerializerSettings.ContractResolver = settings.ContractResolver;
+//            source.SerializerSettings.Converters = settings.Converters;
+//            source.SerializerSettings.DefaultValueHandling = settings.DefaultValueHandling;
+//            source.SerializerSettings.NullValueHandling = settings.NullValueHandling;
 
-            return source;
-        }
-    }
-}
+//            return source;
+//        }
+//    }
+//}
---
+++ tests/Application.Web3/Startup.cs
@@ -0,0 +1,43 @@
+namespace Application.Web3
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<ConsoleLifetimeOptions>(opts => opts.SuppressStatusMessages = true); // https://andrewlock.net/new-in-aspnetcore-3-structured-logging-for-startup-messages/
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
