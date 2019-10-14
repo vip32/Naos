8c0aecb9fe7d0876d1f538b0ed2674e72b72a5b1
Vincent van Proosdij <mini32@gmail.com>
2019-10-14 22:43:24 +0200

    netcore3 httprequest stream read fix

--- src/FileStorage/Domain/Decorators/FileStorageLoggingDecorator.cs
+++ src/FileStorage/Domain/Decorators/FileStorageLoggingDecorator.cs
@@ -27,12 +27,12 @@
 
         private IFileStorage Decoratee { get; }
 
-        public Task<Stream> GetFileStreamAsync(string path, CancellationToken cancellationToken = default)
+        public async Task<Stream> GetFileStreamAsync(string path, CancellationToken cancellationToken = default)
         {
             EnsureArg.IsNotNullOrEmpty(path, nameof(path));
 
             this.logger.LogInformation($"{{LogKey:l}} get {this.name} file stream: {path}", LogKeys.FileStorage);
-            return this.Decoratee.GetFileStreamAsync(path, cancellationToken);
+            return await this.Decoratee.GetFileStreamAsync(path, cancellationToken).AnyContext();
         }
 
         public async Task<FileInformation> GetFileInformationAsync(string path)
@@ -43,53 +43,53 @@
             return await this.GetFileInformationAsync(path).AnyContext();
         }
 
-        public Task<bool> ExistsAsync(string path)
+        public async Task<bool> ExistsAsync(string path)
         {
             EnsureArg.IsNotNullOrEmpty(path, nameof(path));
 
             this.logger.LogInformation($"{{LogKey:l}} exists {this.name} file: {path}", LogKeys.FileStorage);
-            return this.Decoratee.ExistsAsync(path);
+            return await this.Decoratee.ExistsAsync(path).AnyContext();
         }
 
-        public Task<bool> SaveFileAsync(string path, Stream stream, CancellationToken cancellationToken = default)
+        public async Task<bool> SaveFileAsync(string path, Stream stream, CancellationToken cancellationToken = default)
         {
             EnsureArg.IsNotNullOrEmpty(path, nameof(path));
             EnsureArg.IsNotNull(stream, nameof(stream));
 
-            this.logger.LogInformation($"{{LogKey:l}} save {this.name} file: {path} (size={ReadBytes(stream).Length.Bytes().ToString("#.##")})", LogKeys.FileStorage);
-            return this.Decoratee.SaveFileAsync(path, stream, cancellationToken);
+            this.logger.LogInformation($"{{LogKey:l}} save {this.name} file: {path} (size={(await ReadBytesAsync(stream).AnyContext()).Length.Bytes().ToString("#.##")})", LogKeys.FileStorage);
+            return await this.Decoratee.SaveFileAsync(path, stream, cancellationToken).AnyContext();
         }
 
-        public Task<bool> RenameFileAsync(string path, string newPath, CancellationToken cancellationToken = default)
+        public async Task<bool> RenameFileAsync(string path, string newPath, CancellationToken cancellationToken = default)
         {
             EnsureArg.IsNotNullOrEmpty(path, nameof(path));
             EnsureArg.IsNotNullOrEmpty(newPath, nameof(newPath));
 
             this.logger.LogInformation($"{{LogKey:l}} rename {this.name} file: {path} > {newPath}", LogKeys.FileStorage);
-            return this.Decoratee.RenameFileAsync(path, newPath, cancellationToken);
+            return await this.Decoratee.RenameFileAsync(path, newPath, cancellationToken).AnyContext();
         }
 
-        public Task<bool> CopyFileAsync(string path, string targetPath, CancellationToken cancellationToken = default)
+        public async Task<bool> CopyFileAsync(string path, string targetPath, CancellationToken cancellationToken = default)
         {
             EnsureArg.IsNotNullOrEmpty(path, nameof(path));
             EnsureArg.IsNotNullOrEmpty(targetPath, nameof(targetPath));
 
             this.logger.LogInformation($"{{LogKey:l}} copy {this.name} file: {path} > {targetPath}", LogKeys.FileStorage);
-            return this.Decoratee.CopyFileAsync(path, targetPath, cancellationToken);
+            return await this.Decoratee.CopyFileAsync(path, targetPath, cancellationToken).AnyContext();
         }
 
-        public Task<bool> DeleteFileAsync(string path, CancellationToken cancellationToken = default)
+        public async Task<bool> DeleteFileAsync(string path, CancellationToken cancellationToken = default)
         {
             EnsureArg.IsNotNullOrEmpty(path, nameof(path));
 
             this.logger.LogInformation($"{{LogKey:l}} delete {this.name} file: {path}", LogKeys.FileStorage);
-            return this.Decoratee.DeleteFileAsync(path, cancellationToken);
+            return await this.Decoratee.DeleteFileAsync(path, cancellationToken).AnyContext();
         }
 
-        public Task<int> DeleteFilesAsync(string searchPattern = null, CancellationToken cancellationToken = default)
+        public async Task<int> DeleteFilesAsync(string searchPattern = null, CancellationToken cancellationToken = default)
         {
             this.logger.LogInformation($"{{LogKey:l}} delete {this.name} files: {searchPattern}", LogKeys.FileStorage);
-            return this.Decoratee.DeleteFilesAsync(searchPattern, cancellationToken);
+            return await this.Decoratee.DeleteFilesAsync(searchPattern, cancellationToken).AnyContext();
         }
 
         public async Task<PagedResults> GetFileInformationsAsync(int pageSize = 100, string searchPattern = null, CancellationToken cancellationToken = default)
@@ -103,11 +103,11 @@
             this.Decoratee?.Dispose();
         }
 
-        private static byte[] ReadBytes(Stream stream)
+        private static async Task<byte[]> ReadBytesAsync(Stream stream)
         {
             using (var ms = new MemoryStream())
             {
-                stream.CopyTo(ms);
+                await stream.CopyToAsync(ms).AnyContext();
                 //stream.Position = 0;
                 return ms.ToArray();
             }
