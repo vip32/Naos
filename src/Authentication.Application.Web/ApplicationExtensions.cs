e7cdfa8036be8098c5dcc50de89de47ecf98075b
Vincent van Proosdij <mini32@gmail.com>
2020-01-15 00:10:06 +0100

    oidc auth challenge

--- src/Authentication.Application.Web/ApplicationExtensions.cs
+++ src/Authentication.Application.Web/ApplicationExtensions.cs
@@ -25,6 +25,13 @@
             return naosOptions.UseAuthenticationChallenge(new AuthenticationChallengeMiddlewareOptions());
         }
 
+        public static IApplicationBuilder UseAuthenticationChallenge(this IApplicationBuilder builder)
+        {
+            EnsureArg.IsNotNull(builder, nameof(builder));
+
+            return builder.UseAuthenticationChallenge(new AuthenticationChallengeMiddlewareOptions());
+        }
+
         /// <summary>
         /// Enables correlation/request ids for the API request/responses.
         /// </summary>
@@ -35,10 +42,10 @@
             EnsureArg.IsNotNull(naosOptions, nameof(naosOptions));
             EnsureArg.IsNotNull(options, nameof(options));
 
-            var authenticationOptions = naosOptions.Context.Application.ApplicationServices.GetService<IAuthenticationSchemeProvider>();
-            if (authenticationOptions != null)
+            var provider = naosOptions.Context.Application.ApplicationServices.GetService<IAuthenticationSchemeProvider>();
+            if (provider != null)
             {
-                if(authenticationOptions.GetDefaultChallengeSchemeAsync().Result.Name == Authentication.OpenIdConnect.OpenIdConnectDefaults.AuthenticationScheme)
+                if(provider.GetDefaultChallengeSchemeAsync().Result.Name == Authentication.OpenIdConnect.OpenIdConnectDefaults.AuthenticationScheme)
                 {
                     naosOptions.Context.Application.UseMiddleware<AuthenticationChallengeMiddleware>(Options.Create(options));
                     naosOptions.Context.Messages.Add($"{LogKeys.Startup} naos application builder: authentication challenge");
@@ -46,6 +53,24 @@
             }
 
             return naosOptions;
+        }
+
+        public static IApplicationBuilder UseAuthenticationChallenge(this IApplicationBuilder builder, AuthenticationChallengeMiddlewareOptions options)
+        {
+            EnsureArg.IsNotNull(builder, nameof(builder));
+            EnsureArg.IsNotNull(options, nameof(options));
+
+            var provider = builder.ApplicationServices.GetService<IAuthenticationSchemeProvider>();
+            if (provider != null)
+            {
+                if (provider.GetDefaultChallengeSchemeAsync().Result.Name == Authentication.OpenIdConnect.OpenIdConnectDefaults.AuthenticationScheme)
+                {
+                    builder.UseMiddleware<AuthenticationChallengeMiddleware>(Options.Create(options));
+                    //naosOptions.Context.Messages.Add($"{LogKeys.Startup} naos application builder: authentication challenge");
+                }
+            }
+
+            return builder;
         }
 #endif
     }
