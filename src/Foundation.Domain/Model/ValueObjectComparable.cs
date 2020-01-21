623a1294b55d9bfe1f21d7e417c809bf86d14ba2
Vincent van Proosdij <mini32@gmail.com>
2020-01-21 20:38:56 +0100

    valueobject comparable

---
+++ src/Foundation.Domain/Model/ValueObjectComparable.cs
@@ -0,0 +1,110 @@
+﻿namespace Naos.Foundation.Domain
+{
+    using System;
+    using System.Collections.Generic;
+
+    public abstract class ValueObjectComparable : ValueObject, IComparable
+    {
+        public static bool operator ==(ValueObjectComparable left, ValueObjectComparable right)
+        {
+            if (left is null)
+            {
+                return right is null;
+            }
+
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(ValueObjectComparable left, ValueObjectComparable right)
+        {
+            return !(left == right);
+        }
+
+        public static bool operator <(ValueObjectComparable left, ValueObjectComparable right)
+        {
+            return left is null ? right is object : left.CompareTo(right) < 0;
+        }
+
+        public static bool operator <=(ValueObjectComparable left, ValueObjectComparable right)
+        {
+            return left is null || left.CompareTo(right) <= 0;
+        }
+
+        public static bool operator >(ValueObjectComparable left, ValueObjectComparable right)
+        {
+            return left is object && left.CompareTo(right) > 0;
+        }
+
+        public static bool operator >=(ValueObjectComparable left, ValueObjectComparable right)
+        {
+            return left is null ? right is null : left.CompareTo(right) >= 0;
+        }
+
+        public int CompareTo(ValueObjectComparable other)
+        {
+            using (var values = this.GetComparableAtomicValues().GetEnumerator())
+            using (var otherValues = other.GetComparableAtomicValues().GetEnumerator())
+            {
+                while (true)
+                {
+                    var x = values.MoveNext();
+                    var y = otherValues.MoveNext();
+                    if (x != y)
+                    {
+                        throw new InvalidOperationException();
+                    }
+
+                    if (x)
+                    {
+                        var c = values.Current.CompareTo(otherValues.Current);
+                        if (c != 0)
+                        {
+                            return c;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                return 0;
+            }
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (ReferenceEquals(this, obj))
+            {
+                return 0;
+            }
+
+            if (obj is null)
+            {
+                return 1;
+            }
+
+            if (this.GetType() != obj.GetType())
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this.CompareTo(obj as ValueObjectComparable);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        /// <summary>
+        /// Gets the atomic values of the properties important for the equality.
+        /// </summary>
+        protected abstract IEnumerable<IComparable> GetComparableAtomicValues();
+    }
+}
--- tests/Foundation.UnitTests/Domain/Model/ValueObjectTests.cs
+++ tests/Foundation.UnitTests/Domain/Model/ValueObjectTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace Naos.Foundation.UnitTests.Domain
 {
+    using System;
     using System.Collections.Generic;
     using Naos.Foundation.Domain;
     using Shouldly;
@@ -31,11 +32,56 @@
                 City = "New York"
             };
 
-            instance0.Equals(instance1).ShouldBeTrue();
+            instance0.Equals(instance1).ShouldBeTrue(); //IEquatable
             instance0.ShouldBe(instance1);
             instance1.ShouldBe(instance1);
             instance1.ShouldNotBe(instance2);
-            instance0.Equals(instance2).ShouldBeFalse();
+            instance0.Equals(instance2).ShouldBeFalse(); // IEquatable
+        }
+
+        [Fact]
+        public void ComparableTests()
+        {
+            var instance0 = new StubValueObjectComparable
+            {
+                StreetName = "One Microsoft Way",
+                HouseNumber = 1,
+                City = "Seattle",
+                ZipCode = 2222
+            };
+
+            var instance1 = new StubValueObjectComparable
+            {
+                StreetName = "One Microsoft Way",
+                HouseNumber = 1,
+                City = "Seattle",
+                ZipCode = 3333
+            };
+
+            var instance2 = new StubValueObjectComparable
+            {
+                StreetName = "One Microsoft Way",
+                HouseNumber = 1,
+                City = "New York",
+                ZipCode = 1111
+            };
+
+            var instance3 = new StubValueObjectComparable
+            {
+                StreetName = "Second Microsoft Way",
+                HouseNumber = 10,
+                City = "New York",
+                ZipCode = 1111
+            };
+
+            instance0.Equals(instance1).ShouldBeTrue(); //IEquatable
+            instance0.ShouldBe(instance1);
+            instance1.ShouldBe(instance1);
+            instance1.ShouldNotBe(instance2);
+            instance0.Equals(instance2).ShouldBeFalse(); // IEquatable
+            (instance0 > instance2).ShouldBeTrue(); // IComparable
+            (instance2 > instance1).ShouldBeFalse(); // IComparable
+            (instance2 == instance3).ShouldBeTrue(); // IComparable
         }
 
         public class StubValueObject : ValueObject
@@ -51,6 +97,29 @@
                 yield return this.StreetName;
                 yield return this.HouseNumber;
                 yield return this.City;
+            }
+        }
+
+        public class StubValueObjectComparable : ValueObjectComparable
+        {
+            public string StreetName { get; set; }
+
+            public int HouseNumber { get; set; }
+
+            public string City { get; set; }
+
+            public int ZipCode { get; set; }
+
+            protected override IEnumerable<object> GetAtomicValues()
+            {
+                yield return this.StreetName;
+                yield return this.HouseNumber;
+                yield return this.City;
+            }
+
+            protected override IEnumerable<IComparable> GetComparableAtomicValues()
+            {
+                yield return this.ZipCode;
             }
         }
     }
