b93d93d37a41fbbfc936da94b9191727fc1df3a5
Vincent van Proosdij <mini32@gmail.com>
2020-01-25 11:33:20 +0100

    NextOf for dict

--- src/Foundation.Extensions/NextOf.cs
+++ src/Foundation.Extensions/NextOf.cs
@@ -19,5 +19,25 @@
 
             return source.SkipWhile(x => !x.Equals(item)).Skip(1).DefaultIfEmpty(source[0]).FirstOrDefault();
         }
+
+        public static T NextOf<TKey, T>(this IDictionary<TKey, T> source, T item)
+        {
+            if (source.IsNullOrEmpty())
+            {
+                return default;
+            }
+
+            if (item.IsDefault())
+            {
+                return source.FirstOrDefault().Value;
+            }
+
+            if (source.LastOrDefault().Value.Equals(item)) // stop at last item
+            {
+                return default;
+            }
+
+            return source.SkipWhile(x => !x.Value.Equals(item)).Skip(1).FirstOrDefault().Value;
+        }
     }
 }
