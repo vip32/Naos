fdd39894b711b93d70827248ec188157906cd40a
Vincent van Proosdij <mini32@gmail.com>
2020-02-08 01:27:09 +0100

    bf

--- src/Tracing.Infrastructure.Azure/ApplicationInsights/ApplicationInsightsExporter.cs
+++ src/Tracing.Infrastructure.Azure/ApplicationInsights/ApplicationInsightsExporter.cs
@@ -8,7 +8,7 @@
     public class ApplicationInsightsExporter : ISpanExporter
     {
         // TODO: need telemetryclient
-        
+
         public Task ExportAsync(IEnumerable<ISpan> spans, CancellationToken cancellationToken)
         {
             // TODO: implement like https://github.com/open-telemetry/opentelemetry-dotnet/blob/87d0d1eeaa077aa08e408b43ce155449b78d9e47/src/OpenTelemetry.Exporter.ApplicationInsights/ApplicationInsightsTraceExporter.cs#L36
