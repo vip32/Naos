bc596b9fa86724f4bcd2e99f2a45185fecfd0e4e
Vincent van Proosdij <mini32@gmail.com>
2020-02-25 19:43:22 +0100

    policy tests moved to fitness tests project

---
+++ tests/Sample.FitnessTests/Catalogs/ArchitectureFitnessTests.cs
@@ -0,0 +1,12 @@
+﻿namespace Naos.Sample.FitnessTests.Catalogs
+{
+    using Xunit.Abstractions;
+
+    public class ArchitectureFitnessTests : ArchitectureFitnessBaseTests
+    {
+        public ArchitectureFitnessTests(ITestOutputHelper output)
+            : base(output, "Naos.Sample.Catalogs")
+        {
+        }
+    }
+}
---
+++ tests/Sample.FitnessTests/Inventory/ArchitectureFitnessTests.cs
@@ -0,0 +1,12 @@
+﻿namespace Naos.Sample.FitnessTests.Inventory
+{
+    using Xunit.Abstractions;
+
+    public class ArchitectureFitnessTests : ArchitectureFitnessBaseTests
+    {
+        public ArchitectureFitnessTests(ITestOutputHelper output)
+            : base(output, "Naos.Sample.Inventory")
+        {
+        }
+    }
+}
