b9506e823c32aa163bc22d5a89b49632a02178fb
Vincent van Proosdij <mini32@gmail.com>
2020-04-27 15:03:17 +0200

    cc

--- src/Foundation.Infrastructure.Azure.Cosmos/Repositories/Provider/ICosmosSqlProvider{T}.cs
+++ src/Foundation.Infrastructure.Azure.Cosmos/Repositories/Provider/ICosmosSqlProvider{T}.cs
@@ -2,11 +2,8 @@
 {
     using System;
     using System.Collections.Generic;
-    //using System.IO;
-    //using System.Linq;
     using System.Linq.Expressions;
     using System.Threading.Tasks;
-    //using Microsoft.Azure.Documents;
 
     public interface ICosmosSqlProvider<T>
     {
@@ -40,33 +37,5 @@
             object partitionKeyValue = null);
 
         Task<bool> DeleteByIdAsync(string id, object partitionKeyValue = null);
-
-        //Task<T> UpsertAttachmentAsync(T entity, string attachmentId, string contentType, Stream stream);
-
-        //Task<int> CountAsync();
-
-        //Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> expression = null);
-
-        //Task<IEnumerable<T>> GetAllAsync(int count = -1);
-
-        //IEnumerable<string> GetAllIdsBatched(int count = 100);
-
-        //IEnumerable<T> GetAllBatched(int count = 100);
-
-        //Task<Attachment> GetAttachmentByIdAsync(string id, string attachmentId);
-
-        //Task<IEnumerable<string>> GetAttachmentIdsAsync(string id);
-
-        //Task<IEnumerable<Attachment>> GetAttachmentsAsync(string id);
-
-        //Task<Stream> GetAttachmentStreamByIdAsync(string id, string attachmentId);
-
-        //Task<T> LastOrDefaultAsync(Expression<Func<T, bool>> expression = null);
-
-        //Task<IQueryable<T>> QueryAsync(int count = 100);
-
-        //Task<IEnumerable<T>> QueryAsync(string query);
-
-        //Task<bool> DeleteAllAsync();
     }
 }
