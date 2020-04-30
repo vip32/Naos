9e16e21a50f35075e595a6bbcac84c6d15a2f61f
Vincent van Proosdij <mini32@gmail.com>
2020-04-30 17:28:03 +0200

    cc

---
+++ src/Foundation.Infrastructure.EventStore/SqlServer/SqlServerEventStore.cs
@@ -0,0 +1,20 @@
+﻿namespace Naos.Foundation.Infrastructure.EventSourcing
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Naos.Foundation.Domain;
+    using Naos.Foundation.Domain.EventSourcing;
+
+    public class SqlServerEventStore : IEventStore // inspiration: https://github.com/bolicd/eventstore/blob/master/Infrastructure/Repositories/EventStoreRepository.cs
+    {
+        public Task<AppendResult> AppendEventAsync<TId>(string streamName, IDomainEvent<TId> @event)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Event<TId>>> ReadEventsAsync<TId>(string streamName, long? fromVersion = null, long? toVersion = null)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
