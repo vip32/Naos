a360a566168499795c2e04440d994e2e8679ecee
vip32 <mini32@gmail.com>
2020-07-06 23:44:37 +0200

    cc

---
+++ src/Foundation.Domain/Model/IIdentifiable.cs
@@ -0,0 +1,13 @@
+﻿namespace Naos.Foundation.Domain
+{
+
+    public interface IIdentifiable
+    {
+        string IdentifierHash { get; }
+
+        /// <summary>
+        /// Updates the state hash for an entity.
+        /// </summary>
+        void SetIdentifierHash();
+    }
+}
