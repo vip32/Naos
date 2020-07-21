540730bcd9c6a01643b5f321e8bb8c6699363efb
vip32 <mini32@gmail.com>
2020-07-21 09:38:09 +0200

    ReflectionHelper

---
+++ src/Foundation.Utilities/ReflectionHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace Naos.Foundation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+
+    public static class ReflectionHelper
+    {
+        public static void SetProperties(object instance, IDictionary<string, object> propertyItems)
+        {
+            foreach (var propertyInfo in instance.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            {
+                foreach (var propertyItem in propertyItems.Safe())
+                {
+                    var propertyType = Nullable.GetUnderlyingType(propertyInfo.PropertyType) ?? propertyInfo.PropertyType;
+
+                    if (propertyItem.Key.SafeEquals(propertyInfo.Name) && propertyItem.Value != null && propertyInfo.CanWrite)
+                    {
+                        propertyInfo.SetValue(instance, propertyItem.Value.To(propertyType), null);
+                    }
+                }
+            }
+        }
+    }
+}
